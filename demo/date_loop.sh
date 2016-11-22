8f48314b1e22a1f7f9811f0a717da42d19719ecd
mumingv <mumingv@163.com>
2016-11-22 17:24:35 +0800

    根据日期进行循环

---
+++ demo/date_loop.sh
@@ -0,0 +1,22 @@
+#/***********************************************************
+#      FileName: date_loop.sh
+#          Desc: 根据日期进行循环
+#        Author: Jie Yin
+#         Email: mumingv@163.com
+#      HomePage: https://github.com/mumingv
+#       Version: 0.0.1
+#    LastChange: 2016-11-22 17:07:18
+#       History:
+# ***********************************************************/
+#/bin/bash
+
+start="2016-10-01" 
+end="2016-11-21"
+
+
+end=`date -d "1 day ${end}" +%Y-%m-%d`    # 日期自增
+while [[ ${start} < ${end} ]]
+do
+  echo ${start}
+  start=`date -d "1 day ${start}" +%Y-%m-%d`    # 日期自增
+done
