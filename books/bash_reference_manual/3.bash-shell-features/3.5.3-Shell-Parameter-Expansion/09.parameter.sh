964386394f3bbbcea92c93f982d8387ef8038d61
mumingv <mumingv@163.com>
2018-07-09 07:36:56 +0800

    add

--- books/bash_reference_manual/3.bash-shell-features/3.5.3-Shell-Parameter-Expansion/09.parameter.sh
+++ books/bash_reference_manual/3.bash-shell-features/3.5.3-Shell-Parameter-Expansion/09.parameter.sh
@@ -7,3 +7,7 @@ echo ${string##0123}  # 456789abcdefh
 string=0123456789abcdefh
 echo ${string%defh}  # 0123456789abc
 echo ${string%%defh}  # 0123456789abc
+
+string=0123456789abcdefh
+echo ${string/abc/xyz}  # 0123456789xyzdefh
+
