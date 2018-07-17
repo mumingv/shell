c716f79854468f3a0426544869b509824e6a6d45
mumingv <mumingv@163.com>
2018-07-18 00:13:52 +0800

    add

--- books/bash_reference_manual/3.bash-shell-features/3.5.3-Shell-Parameter-Expansion/09.parameter.sh
+++ books/bash_reference_manual/3.bash-shell-features/3.5.3-Shell-Parameter-Expansion/09.parameter.sh
@@ -11,3 +11,8 @@ echo ${string%%defh}  # 0123456789abc
 string=0123456789abcdefh
 echo ${string/abc/xyz}  # 0123456789xyzdefh
 
+string=0123456789abcdefh
+echo ${string^abc}  # 大小写转换，Mac不支持
+echo ${string,abc}  # 大小写转换，Mac不支持
+
+
